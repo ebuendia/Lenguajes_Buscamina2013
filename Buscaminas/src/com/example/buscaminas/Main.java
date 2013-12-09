0c6acced94d16f933d9aad530bf6510c52d98206
joseluisbueno <jbueno@espol.edu.ec>
2013-12-09 00:10:44 -0500

    Creado el archivo Start.java el cual tiene la funcionalidad del splash screen

---
+++ Buscaminas/src/com/example/buscaminas/Main.java
@@ -0,0 +1,16 @@
+package com.example.buscaminas;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Main extends Activity {
+
+	@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setContentView(R.layout.main);
+        
+        
+	}
+
+}
--- Buscaminas/src/com/example/buscaminas/Start.java
+++ Buscaminas/src/com/example/buscaminas/Start.java
@@ -1,8 +1,11 @@
 package com.example.buscaminas;
 
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.widget.Chronometer;
 
 public class Start extends Activity {
 
@@ -10,13 +13,26 @@ public class Start extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.start);
+		
+		final Chronometer cr = (Chronometer)findViewById(R.id.cronometro);
+		
+		
+		cr.setBase(SystemClock.elapsedRealtime());
+		cr.start();
+		cr.setOnChronometerTickListener(new Chronometer.OnChronometerTickListener(){
+
+			@Override
+			public void onChronometerTick(Chronometer chronometer) {
+				// TODO Auto-generated method stub
+				if (cr.getText().equals("00:03")){
+					cr.stop();
+					Intent i = new Intent(Start.this,Main.class);
+					startActivity(i);
+				}
+			}
+			
+		});
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.start, menu);
-		return true;
-	}
 
 }
