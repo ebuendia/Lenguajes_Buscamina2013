a21f2b355619cfba860252472a4f1d0b5c686ac2
ebuendia <ebuendia@espol.edu.ec>
2013-12-09 00:57:56 -0500

    se creo el activity About.java y se la registro al archivo androidmanifest.xml

---
+++ Buscaminas/src/com/example/buscaminas/About.java
@@ -0,0 +1,14 @@
+package com.example.buscaminas;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class About extends Activity {
+	
+	protected void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		this.setContentView(R.layout.about);
+	
+	}
+	
+}
