dbd94ae733dc1ec4db81b2b611be847aa881cb8d
joseluisbueno <jbueno@espol.edu.ec>
2013-12-13 14:12:16 -0500

    Añadido el layout de Rankings y su activity Ranking.java

---
+++ Buscaminas/src/com/example/buscaminas/Ranking.java
@@ -0,0 +1,21 @@
+package com.example.buscaminas;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class Ranking extends Activity {
+	protected void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.ranking);
+				
+	}
+	
+	public void loadThisOnCreate(){
+		
+		
+	}
+	
+}
